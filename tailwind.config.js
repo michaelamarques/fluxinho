82e81ea5dd1e1f5a79b78c7ce4b0d637cedcf3c7
michaelamarques <michaelasilvamarques@gmail.com>
2026-05-23 18:42:41 -0300

    Atualizar o tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}"
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/styles/**/*.{css}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rosa: "#F7C6D0",
+        creme: "#FFF6F0",
+        lilas: "#D9C7F3",
+        cinza: "#EAE4E1",
+      },
+    },
   },
   plugins: [],
 }
